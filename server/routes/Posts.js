e0b9b219d54f529391a324c7d1970f22df7638a0
Himanshu Srivastava <84365673+himanshuSri24@users.noreply.github.com>
2023-01-07 00:36:21 +0530

    Add files via upload

--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { Posts } = require("../models");
+const {validateToken} = require('../middleware/AuthMiddleware')
 
 router.get("/", async (req, res) => {
   const listOfPosts = await Posts.findAll();
@@ -13,7 +14,7 @@ router.get("/byId/:id", async (req, res) => {
   res.json(post);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", validateToken, async (req, res) => {
   const post = req.body;
   await Posts.create(post);
   res.json(post);
