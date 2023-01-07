d73f4b9227b7a4b487ca84f40e01390b0cb8d43a
Anshus09 <satijaa21@gmail.com>
2023-01-07 09:47:48 +0530

    anshu v1

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,7 @@ function App() {
           <Route path="/register" element = {<Register/>} />
         </Routes>
       </Router>
+      
     </div>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -19,8 +19,12 @@ function Home() {
             <div className="title"> {value.title} </div>
             <div className="body">{value.postText}</div>
             <div className="footer">{value.username}</div>
-          </div>
+          </div>  
         );
+        <img
+        src="https://bobbyhadz.com/images/blog/react-prevent-multiple-button-clicks/thumbnail.webp"
+        alt="car"
+      />
       })}</div>
   )
 }
